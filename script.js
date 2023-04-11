fc8db12be4fda3df8e2b49c77fb344f07b681919
NicPix30 <117862776+NicPix30@users.noreply.github.com>
2023-04-11 14:28:20 -0500

    1 file modified, added smoother movement

--- script.js
+++ script.js
@@ -29,6 +29,25 @@ class Sprite {
   }
 }
 
+class Player {
+  constructor({ position, size, imageSrc }) {
+    this.image = new Image();
+    this.image.src = imageSrc;
+    this.position = position;
+    this.size = size;
+    this.imageSrc = imageSrc;
+  }
+  draw() {
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.size.width,
+      this.size.height
+    );
+  }
+}
+
 //basic background image
 const backgroundLevel1 = new Sprite({
   position: {
@@ -43,4 +62,62 @@ const backgroundLevel1 = new Sprite({
   },
 });
 
-backgroundLevel1.draw();
+//basic player
+const player1 = new Player({
+  position: {
+    x: canvas.width / 2,
+    y: 362,
+  },
+  imageSrc:
+    'https://cdn.jsdelivr.net/gh/NicPix30/2D-Game-Template@main/box.png',
+  size: {
+    width: 50,
+    height: 50,
+  },
+});
+
+//____________________________________________________________________________________________________________________________________
+//                                              Controls User Input on the WASD Keys
+//                 This is done to make movement and other actions smoother instead of using normal keypress which stutters
+const TickRate = 30,
+  keyDown = {},
+  keyMap = {
+    87: 'up',
+    65: 'left',
+    83: 'down',
+    68: 'right',
+  };
+
+window.addEventListener('keydown', (event) => {
+  keyDown[keyMap[event.which]] = true;
+});
+
+window.addEventListener('keyup', (event) => {
+  keyDown[keyMap[event.which]] = false;
+});
+
+var tick = function () {
+  if (keyDown['up']) {
+    // up code
+  } else if (keyDown['down']) {
+    // down code
+  } else if (keyDown['left']) {
+    player1.position.x -= 10;
+  } else if (keyDown['right']) {
+    player1.position.x += 10;
+  }
+
+  setTimeout(tick, TickRate);
+};
+
+tick();
+//____________________________________________________________________________________________________________________________________
+
+function coreAnimationLoop() {
+  window.requestAnimationFrame(coreAnimationLoop);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  backgroundLevel1.draw();
+  player1.draw();
+}
+
+coreAnimationLoop();
